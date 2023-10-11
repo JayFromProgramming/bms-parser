51c15bc02bfa0af29797076d32227f22ef92c6d6
JayFromProgramming <ajsweene@mtu.edu>
2023-10-11 13:38:35 -0400

    Developing bluetooth interface

--- py/helper/data.py
+++ py/helper/data.py
@@ -131,7 +131,7 @@ class Serial:
                 print(f"  {c}")
         # Get the service and characteristic UUIDs
         service_uuid = "0000ff0-0000-1000-8000-00805f9b34fb"
-        rx = "0000ff01-0000-1000-8000-00805f9b34fb"
+        rx = "0000180a-0000-1000-8000-00805f9b34fb"
         tx = "0000ff02-0000-1000-8000-00805f9b34fb"
         # paired = await self.client.pair(legacy=True)
         # print(f"Paired: {paired}")
